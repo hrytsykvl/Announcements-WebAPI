2aa4e385e29db08bd73a7072b8779afc7a01f69b
hrytsykvl <gricikvlad@gmail.com>
2024-08-14 12:24:40 +0300

    Add project files.

---
+++ Announcements.WebAPI/Controllers/AnnouncementsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Announcements.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnnouncementsController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello";
+        }
+    }
+}
